b548a5cfc96747fa8a94e9a01debe44a1d5d7dd9
shadow578 <christophnienhaus2@gmail.com>
2020-02-04 17:28:30 +0100

    Should work OK now

---
+++ FindString/IOUtil.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FindString
+{
+    public static class IOUtil
+    {
+        /// <summary>
+        /// Get the parent directory
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <returns></returns>
+        public static DirectoryInfo GetParent(this DirectoryInfo dir)
+        {
+            return Directory.GetParent(dir.FullName);
+        }
+
+        /// <summary>
+        /// Get the directory this file is in
+        /// </summary>
+        /// <param name="file">the file</param>
+        /// <returns></returns>
+        public static DirectoryInfo GetDirectory(this FileInfo file)
+        {
+            return new DirectoryInfo(Path.GetDirectoryName(file.FullName));
+        }
+
+        /// <summary>
+        /// Creates a file in the directory
+        /// <para>if the file already exists, the file is opened instead</para>
+        /// </summary>
+        /// <param name="dir">the dir</param>
+        /// <param name="filename">the name of the file</param>
+        /// <param name="createIfNotExisting">should file.create be called if the file does not exist?</param>
+        /// <returns>the file</returns>
+        public static FileInfo CreateFile(this DirectoryInfo dir, string filename, bool createIfNotExisting = true)
+        {
+            FileInfo file = new FileInfo(dir.FullName + "\\" + filename);
+
+            if (!file.Exists && createIfNotExisting)
+            {
+                file.Create();
+            }
+
+            return file;
+        }
+
+        /// <summary>
+        /// Is this directory empty?
+        /// </summary>
+        /// <param name="dir">the dir</param>
+        /// <returns>empty?</returns>
+        public static bool IsEmpty(this DirectoryInfo dir)
+        {
+            return !dir.Exists || (dir.GetFiles().Length == 0 && dir.GetDirectories().Length == 0);
+        }
+
+        /// <summary>
+        /// Returns the path of the file relative to the base directory
+        /// <para>The file has to be inside the base directory</para>
+        /// </summary>
+        /// <param name="file">the file</param>
+        /// <param name="baseDirPath">the base direcotry path</param>
+        /// <returns>a path relative to the base dir</returns>
+        public static string GetRelativePath(this FileInfo file, string baseDirPath)
+        {
+            //check if file exists
+            if (!file.Exists)
+            {
+                throw new FileNotFoundException("Cannot create relative path: file not found");
+            }
+
+            //check if file path begins with base path
+            string filePath = file.FullName;
+            if (!filePath.ToLower().StartsWith(baseDirPath.ToLower()))
+            {
+                throw new InvalidOperationException("Cannot create relative path: The file is not inside the base directory");
+            }
+
+            return filePath.Substring(baseDirPath.Length + 1);
+        }
+
+        /// <summary>
+        /// Searches a directory and all subdirectorys for files and returns a list of all files
+        /// </summary>
+        /// <param name="dir">the directory to start searching in</param>
+        /// <param name="maxSubdirLevel">how many levels of sub dirs to search</param>
+        /// <param name="level">internally used only. Leave as 0</param>
+        /// <returns></returns> a list of all found files
+        public static List<FileInfo> GetFilesIncludingSubdirs(this DirectoryInfo dir, int maxSubdirLevel = 15, int level = 0)
+        {
+            //init filepath and count up anti-stack-overflow var
+            List<FileInfo> filePaths = new List<FileInfo>();
+            level++;
+
+            //check if dir exists
+            if (!dir.Exists)
+            {
+                return filePaths;
+            }
+
+            //search subdirs for files if not at max search level
+            if (level < maxSubdirLevel)
+            {
+                foreach (DirectoryInfo subDir in dir.GetDirectories())
+                {
+                    filePaths.AddRange(GetFilesIncludingSubdirs(subDir, maxSubdirLevel, level));
+                }
+            }
+
+            //search for files
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                filePaths.Add(file);
+            }
+
+            return filePaths;
+        }
+
+        /// <summary>
+        /// Gets the file size of the file
+        /// </summary>
+        /// <param name="file">the file</param>
+        /// <returns>the filesize in bytes</returns> 
+        public static long GetFileSize(this FileInfo file)
+        {
+            return file.Length;
+        }
+    }
+}
--- FindString/Program.cs
+++ FindString/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//may not work
+//#define USE_PARALLEL_SEARCH
+
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FindString
@@ -10,6 +12,153 @@ namespace FindString
     {
         static void Main(string[] args)
         {
+            DirectoryInfo searchDir;
+            StreamWriter logFileWriter = null;
+            string searchPattern;
+            bool searchPatternIsRegex;
+            bool shouldLogToFile;
+
+            #region Setup Runtime Vars by user
+            //get search directory
+            do
+            {
+                searchDir = new DirectoryInfo(UserGetString("Enter Directory to Search in: "));
+            } while (!searchDir.Exists);
+
+            //get search pattern
+            searchPattern = UserGetString("Enter Pattern to Search for (supports RegEx): ");
+
+            //ask for regex
+            searchPatternIsRegex = UserGetString("Does the Search Pattern use RegEx? (y/n): ").ToLower().Contains("y");
+
+            //ask for log to file
+            shouldLogToFile = UserGetString("Do you want to log to a file? (y/n): ").ToLower().Contains("y");
+
+            //ask for log file path
+            if (shouldLogToFile)
+            {
+                string logFilePath;
+                do
+                {
+                    logFilePath = UserGetString("Enter Log file Path: ");
+                } while (!TryCreateTextFile(logFilePath, out logFileWriter));
+            }
+            #endregion
+
+            //all set- up now, start the search!
+            int matches = 0;
+            FindStringIn(searchDir, (file, lineNr, line) =>
+            {
+                //count this match
+                matches++;
+
+                //get immediate characters AROUND match pattern (only support non- regex for now)
+                string immediateStr;
+                if (!searchPatternIsRegex)
+                {
+                    int matchStartIndex = line.IndexOf(searchPattern, StringComparison.InvariantCultureIgnoreCase);
+                    int immediateStrLength = searchPattern.Length + 20;
+                    if ((matchStartIndex + immediateStrLength) >= line.Length)
+                    {
+                        immediateStrLength = (line.Length - matchStartIndex - 1);
+                    }
+
+                    immediateStr = line.Substring(matchStartIndex, immediateStrLength);
+                }
+                else
+                {
+                    //not supporting regex for now
+                    immediateStr = "";
+                }
+
+                //write info to console
+                Console.WriteLine($"Match in \"{file.Name}\" line {lineNr}: {immediateStr}");
+
+                //write to log file if enabled
+                if (shouldLogToFile && logFileWriter != null)
+                {
+                    logFileWriter.WriteLine($"Match in \"{file.Name}\" line {lineNr}: {(string.IsNullOrWhiteSpace(immediateStr) ? line : immediateStr)}");
+                }
+            }, searchPattern, searchPatternIsRegex);
+
+            //search ended
+            logFileWriter?.Close();
+            Console.WriteLine($"Search finished! Found {matches} Matches.");
+            Console.ReadLine();
+        }
+
+        static string UserGetString(string msg)
+        {
+            string o;
+            do
+            {
+                Console.Write(msg);
+                o = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(o));
+            return o;
+        }
+
+        static bool TryCreateTextFile(string path, out StreamWriter writer)
+        {
+            //dummy writer
+            writer = null;
+
+            //abort if exists
+            if (File.Exists(path)) return false;
+
+            try
+            {
+                writer = File.CreateText(path);
+                return true;
+            }
+            catch (Exception)
+            {
+                //failed
+                return false;
+            }
+        }
+
+        static void FindStringIn(DirectoryInfo searchDir, Action<FileInfo /*matched file*/, int /*line*/, string /*line contents*/> matchAction, string searchFor, bool useRegex)
+        {
+#if USE_PARALLEL_SEARCH
+            Parallel.ForEach(searchDir.GetFilesIncludingSubdirs(), (file) =>
+#else
+            foreach (FileInfo file in searchDir.GetFilesIncludingSubdirs())
+#endif
+            {
+                using (StreamReader sr = file.OpenText())
+                {
+                    int lnNum = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        string ln = sr.ReadLine();
+                        lnNum++;
+
+                        if (MatchLine(ln, searchFor, useRegex))
+                        {
+                            //found one!
+                            matchAction(file, lnNum, ln);
+                        }
+                    }
+                }
+#if USE_PARALLEL_SEARCH
+            });
+#else
+            }
+#endif
+        }
+
+        static bool MatchLine(string line, string searchFor, bool useRegex)
+        {
+            if (useRegex)
+            {
+                Regex regex = new Regex(searchFor);
+                return regex.IsMatch(line);
+            }
+            else
+            {
+                return line.ToLower().Contains(searchFor.ToLower());
+            }
         }
     }
 }
